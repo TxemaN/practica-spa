47c568ffba0f5e9c4a8a3b53f48cb113f45bb3c6
navarrorafa <115833775+navarrorafa@users.noreply.github.com>
2023-09-20 17:22:06 +0200

    add isActive

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,25 +3,28 @@ import { NavLink } from 'react-router-dom';
 export const NavBar = () => {
 
   const evento = () => {
-
+ //NO OLVIDAR DE PONER A CLASE DE LOS BOTONES NO CSS 
   }
   return (
      <nav>
       <ul>
         <li>
           <NavLink
-          to='/'>
+           className={({ isActive }) => `nav-link ${isActive ? 'active' : ''} `}
+          to='/' >
           Home
           </NavLink>
         </li>
         <li>
           <NavLink
+           className={({ isActive }) => `nav-link ${isActive ? 'active' : ''} `}
           to='/Mobile'>
            Mobile 
           </NavLink>
         </li>
         <li>
           <NavLink
+           className={({ isActive }) => `nav-link ${isActive ? 'active' : ''} `}
           to='/Muebles'>
            Muebles
           </NavLink>
