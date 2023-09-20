8bf8484fdfee243cbeb81e647e9788b89a55bb96
carlOzil <seifer01es@msn.com>
2023-09-20 22:51:42 +0200

    helpers in

--- src/productos/helpers/getCategoria.js
+++ src/productos/helpers/getCategoria.js
@@ -4,7 +4,7 @@ export const getCategoria = async (categoria) => {
     const urlBase = import.meta.env.VITE_APP_URL;
     const urlCategoria = `${urlBase}/category/${categoria}`;
 
-    const { data } = await dummyConsulta(urlCategoria, 'GET');
+    const data = await dummyConsulta(urlCategoria, 'GET');
 
     const products = data.map((prod) => ({
         id: prod.id,
