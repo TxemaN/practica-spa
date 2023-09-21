f9bc7ba57a1a970b0e9f99e19a788d2af8209dda
navarrorafa <115833775+navarrorafa@users.noreply.github.com>
2023-09-21 22:59:01 +0200

    app working 3

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,15 @@ function App() {
   
   return (
     <>
+    <header className="bg-dark">
+      <p className="text-white">Practica SPA </p>
+    </header>
     <UserProvider>
      <AppRouter/>
      </UserProvider>
+     <footer className="bg-dark">
+      <p>Txema , Carlos , Rafael - 2023</p>
+     </footer>
     </>
   )
 }
--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import { PintarProducto } from '../productos/components/PintarProducto';
+import { NavBar } from '../components/NavBar';
 
 export const ProductPage = () => {
-  return <PintarProducto />;
+
+  return(
+   <>
+  <NavBar/>
+  <PintarProducto />
+  </> 
+  )
 };
